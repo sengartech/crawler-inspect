b4ca5c440efd5b0b01ff120d082013721abbe402
Rishabh Singh Sengar <sengartech@gmail.com>
2019-02-18 17:03:13 +0530

    initial commit

---
+++ src/detector.js
@@ -0,0 +1,22 @@
+/**
+ * importing modules.
+ */
+const config = require('./config.json')
+
+let allowedIpsList = []
+
+/**
+ * function to check given ip is crawler or not.
+ * return true if crawler, spider, bot.
+ * params: ipAddress
+ */
+let isCrawler = (ipAddress) => {
+
+} // end of the isCrawler function.
+
+/**
+ * exporting function.
+ */
+module.exports = {
+  isCrawler
+}
